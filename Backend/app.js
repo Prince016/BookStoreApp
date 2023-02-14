da4013476de46d6def7977e07fc61a3afda4be0b
Prince016 <prashantsharma3854@gmail.com>
2023-02-14 23:56:51 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const router = require('./routes/book-routes');
+const cors = require("cors");
+const app = express();
+
+
+//Middlewares 
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+
+mongoose.connect("mongodb+srv://rock:rock@cluster0.esdi0ly.mongodb.net/bookStore?retryWrites=true&w=majority")
+    .then(() => console.log("Connected to Database"))
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch((err) => console.log(err));
